db8a61445f99c6175c87e8b8d6d8307a2dda295f
Sanskruti14 <103481196+Sanskruti14@users.noreply.github.com>
2026-06-11 10:48:21 +0530

    Add more numbers to the list in main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-numbers = [42, 17, 89, 33, 10, 55, 78, 21, 94, 6]
+numbers = [42, 17, 89, 33, 10, 55, 78, 21, 94, 6,3,65,78,34,14,57,98,08,12,76]
 
 highest = max(numbers)
 lowest = min(numbers)
