07cd1a6fd1d577815abd58faf042b3ce16dab91e
Sanskruti14 <103481196+Sanskruti14@users.noreply.github.com>
2026-06-11 11:11:50 +0530

    Add script to deploy and execute app on EC2

---
+++ deploy_to_ec2.py
@@ -0,0 +1,27 @@
+import paramiko
+
+host = " 13.233.225.252 "
+
+username = "ubuntu"
+
+key = "sanu.pem"
+
+ssh = paramiko.SSHClient()
+
+ssh.set_missing_host_key_policy(
+    paramiko.AutoAddPolicy()
+)
+
+ssh.connect(
+    host,
+    username=username,
+    key_filename=key
+)
+
+stdin, stdout, stderr = ssh.exec_command(
+    "python3 app.py"
+)
+
+print(stdout.read().decode())
+
+ssh.close()
